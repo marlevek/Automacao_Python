f3830a746115bf7a0a7224a9fba03a2617f18a4c
Marcelo Zagonel Levek <marcelo_levek@hotmail.com>
2022-01-17 11:28:34 -0300

    Update automat_app_musica.py

--- automat_app_musica.py
+++ automat_app_musica.py
@@ -7,3 +7,13 @@ pya.press('win')
 sleep(0.5)
 pya.write('tunein')
 pya.press('enter')
+
+sleep(3)
+
+pya.click(x=531, y=476, clicks=2)  # escolhendo a rádio LOVETIMES
+sleep(1.5)
+
+pya.click(x=1848, y=444)   # clicando para começar a tocar
+sleep(2)
+
+pya.click(x=1799, y=10)  # minimizando a janela
